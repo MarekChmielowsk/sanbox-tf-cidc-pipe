b2e954f8c3e84bd7dd07df3974fe0d08ced8d033
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-18 11:40:15 +0100

    Create test VM

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "google" {
 
 # Create a single Compute Engine instance
 resource "google_compute_instance" "default" {
-  name         = "flask-vm"
+  name         = "test-vm"
   machine_type = "f1-micro"
   zone         = "us-central1-a"
   tags         = ["ssh"]
