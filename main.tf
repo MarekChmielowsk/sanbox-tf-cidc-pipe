29f9ef5937820b2f99a5cf8855121c21413fa598
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-18 15:29:30 +0100

    [Apply_Changes]

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "google_compute_network" "vpc_network" {
 resource "google_compute_subnetwork" "default" {
   name          = "my-custom-subnet"
   ip_cidr_range = "10.0.1.0/24"
-  region        = "us-west1"
+  region        = "us-central1"
   network       = google_compute_network.vpc_network.id
 }
 
