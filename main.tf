86e9d67c55a1a13117cb94d33c6ce3fc9ffa063d
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-18 15:18:22 +0100

    [Apply_Changes]

--- main.tf
+++ main.tf
@@ -8,6 +8,20 @@ provider "google" {
 # To apply   - to commit message add => [Apply_Changes]  
 # To destroy - to commit message add => [Destroy_All] 
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name = "test-bucket-random-001123"
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
+  }
 }
