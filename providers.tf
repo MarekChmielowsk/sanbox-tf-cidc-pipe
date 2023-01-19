6b377322658cae6a5c7211a80463bd3c409405d6
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-19 15:48:37 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,9 +4,9 @@ provider "google" {
   zone    = var.zone
 }
 
-terraform {
-  backend "gcs" {
-    bucket = "sandbox-marek-260185-tfstate"
-    prefix = "terraform/state"
-  }
-}
+#terraform {
+#  backend "gcs" {
+#    bucket = "sandbox-marek-260185-tfstate"
+#    prefix = "terraform/state"
+#  }
+#}
