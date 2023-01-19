5f5221ee578c9d484f9d99035ef54f3439a462d1
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-19 15:32:56 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+# Should be defined in terraform.tfvars
+variable "project_id" {
+  type        = string
+  description = "The Google Cloud Project Id"
+}
+
+# us-central1 is chepeast region
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type        = string
+  default = "us-central1-c"
+}
