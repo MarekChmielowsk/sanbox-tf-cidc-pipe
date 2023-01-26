17e3e9d32a45d4a02d5c37a20682358fa03ea341
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-26 15:31:37 +0100

    Create main.tf

---
+++ modules/gcp_internal_bucket/main.tf
@@ -0,0 +1,8 @@
+
+# To apply   - to commit message add => [Apply_Changes]  
+# To destroy - to commit message add => [Destroy_All]
+
+resource "google_storage_bucket" "bucket_internal" {
+  name     = "test-bucket-random-260185-23"
+  location = var.region
+}
