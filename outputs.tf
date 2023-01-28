96cf71cd43c93452f1de2751c942fdaf37047bf3
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 13:25:55 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "rnd_str_6_result" {
   description = "Random String bucket params"
-  value       = "Random String: ${rnd_str_6.result} length at ${rnd_str_6.length}"
+  value       = "Random String: ${random_string.rnd_str_6.result} length at ${random_string.rnd_str_6.length}"
 }
 
 output "test_bucket_name" {
