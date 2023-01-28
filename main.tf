222a35b3307f59a8ae9baec8b66ef2f6430b91ad
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 11:57:49 +0100

    [Destroy_All]

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 # To apply   - to commit message add => [Apply_Changes]  
-# To destroy - to commit message add => [Destroy_All]   
+# To destroy - to commit message add => [Destroy_All]    
 
 # resource "google_storage_bucket" "bucket_test" {
 #   name     = "test-bucket-random-260185-23"
