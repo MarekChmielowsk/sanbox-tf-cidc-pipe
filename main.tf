b2e183cc6408baf37acfb15d4866d11bf2f16941
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 11:33:38 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,11 +7,17 @@
 #   location = var.region
 # }
 
+
+resource "random_string" "rnd_str_6" {
+  length  = 6
+  special = false
+}
+
 module "internal_bucket" {
   source = "./modules/gcp-internal-bucket"
   # required input param
   # bucket_name = "test-bucket-random-260185-23"
-  bucket_name = "test-${var.project_nr}-260185-23"
+  bucket_name = "test-pr-nr-${var.project_nr}-rnd-${random_string.rnd_str_6.result}-time-${formatdate("YYYYMMDDhhmmss", timestamp())}"
   bucket_location = var.region
 }
 
