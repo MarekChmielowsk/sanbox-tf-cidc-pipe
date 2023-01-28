4bbdd2f233a03f89760620f385eb681c9aac2993
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 13:04:47 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "rnd_str_6_result" {
+  description = "Random String bucket params"
+  value       = "Random String: ${module.rnd_str_6.result} length at ${module.rnd_str_6_bucket.length}"
+}
+
 output "test_bucket_name" {
   description = "Test bucket namer"
   value       = "Bucket: ${module.internal_bucket.name} created at ${module.internal_bucket.location}"
