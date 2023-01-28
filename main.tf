e85d3938b4d5f253a5bdc5e6c9ec2fbfab6c0898
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 10:17:14 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@
 # To apply   - to commit message add => [Apply_Changes]  
 # To destroy - to commit message add => [Destroy_All]
 
-#resource "google_storage_bucket" "bucket_test" {
+# resource "google_storage_bucket" "bucket_test" {
 #  name     = "test-bucket-random-260185-23"
 #  location = var.region
-#}
+# }
 
 module "internal_bucket" {
   source = "./modules/gcp-internal-bucket"
