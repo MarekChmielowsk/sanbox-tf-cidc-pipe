9ae0b6ba1f660f53a5d6b37ca4c2b0ec3f6bad0b
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 11:55:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,9 @@ resource "random_string" "rnd_str_6" {
 module "internal_bucket" {
   source = "./modules/gcp-internal-bucket"
   # required input param
+  # this will create only one test bucket
   # bucket_name = "test-bucket-random-260185-23"
+  # this will
   bucket_name = lower("test-pr-nr-${var.project_nr}-rnd-${random_string.rnd_str_6.result}-time-${formatdate("YYYYMMDDhhmmss", timestamp())}")
   bucket_location = var.region
 }
