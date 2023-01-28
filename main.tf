173c533fad280710d41fa43bb36ba4a187e49cdb
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 10:27:36 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 # To destroy - to commit message add => [Destroy_All]
 
 # resource "google_storage_bucket" "bucket_test" {
-#  name     = "test-bucket-random-260185-23"
-#  location = var.region
+#   name     = "test-bucket-random-260185-23"
+#   location = var.region
 # }
 
 module "internal_bucket" {
