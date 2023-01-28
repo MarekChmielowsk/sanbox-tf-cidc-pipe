95b140bf295d9c486c84034fda4dac48f07588f1
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 19:15:43 +0100

    Update outputs.tf

--- modules/gcp-internal-bucket/outputs.tf
+++ modules/gcp-internal-bucket/outputs.tf
@@ -1,5 +1,5 @@
 output "internal_bucket_name_location" {
   description = "internal bucket name + locatioon"
   # value = "${google_storage_bucket.bucket_internal.name} at ${google_storage_bucket.bucket_internal.location}"
-  value = [for bucket in google_storage_bucket.bucket_internal : "${google_storage_bucket.bucket.name} at ${google_storage_bucket.bucket.location} | "]
+  value = [for bucket_internal in google_storage_bucket.bucket_internal : "${google_storage_bucket.bucket_internal.name} at ${google_storage_bucket.bucket_internal.location} | "]
 }
