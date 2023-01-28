2e5f62470f251a02e1fe16ea4064e74820aefa42
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-28 19:41:01 +0100

    Update main.tf

--- modules/gcp-internal-bucket/main.tf
+++ modules/gcp-internal-bucket/main.tf
@@ -3,7 +3,7 @@
 # To destroy - to commit message add => [Destroy_All]
 
 resource "google_storage_bucket" "bucket_internal" {
-  count    = var.bucket_counts
+  count    = var.bucket_count
   name     = "internal-${var.bucket_name}-${count.index}"
   location = var.bucket_location
 }
