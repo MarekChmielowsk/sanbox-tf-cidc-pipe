98b9352df603bc33520002c400ae0a6664cf6752
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-01-29 19:05:51 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,11 +18,11 @@ resource "random_string" "rnd_str_6" {
 module "internal_bucket" {
   source = "./modules/gcp-internal-bucket"
   # required input param
-  # this will create only one test bucket
+  # this will create one or more test bucket on first run and re-create bucket(s) with the same name(s) on subsequent runs terraform apply
   bucket_name = "test-bucket-fixed-260185"
-  # this will create a new test bucket on every apply
+  # this will create a new test bucket(s) on every terraform apply
   # bucket_name = lower("test-pr-nr-${var.project_nr}-rnd-${random_string.rnd_str_6.result}-time-${formatdate("YYYYMMDDhhmmss", timestamp())}")
-  bucket_location = var.region
+  bucket_location = var.region  # Or hardcode region
   # optional
   bucket_count = 3
 }
