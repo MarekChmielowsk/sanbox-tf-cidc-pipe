070910870437adeb4be7321dcf9658018035fc1c
MarekChmielowsk <116885033+MarekChmielowsk@users.noreply.github.com>
2023-03-08 15:08:42 +0100

    [Destroy_All]

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
 # To apply   - just merge/push to main  
 # To destroy - to merge/push to main with commit message contains => [Destroy_All]   
 # test update 8th March 2023 14:23 CET DESTROYED
+# test update 8th March 2023 15:08 CET DESTROYED
 
 # resource "google_storage_bucket" "bucket_test" {
 #   name     = "test-bucket-random-260185-23"
